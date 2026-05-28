6136c1096e2da92adad6abda3ed0efe419bc5047
Peter Sutton <peter.sutton@ocado.com>
2026-05-28 21:17:11 +0100

    Bump version due to HACS cache issues.

--- custom_components/ha-microclimate/const.py
+++ custom_components/ha-microclimate/const.py
@@ -13,7 +13,7 @@ from homeassistant.components.switch import (
 )
 
 DOMAIN: Final = "ha_microclimate"
-VERSION: Final = "0.0.2"
+VERSION: Final = "0.0.3"
 MANUFACTURER: Final = "Microclimate"
 
 # Configuration
